6c7cb88164c94dc13828077e1b70622b248080af
bi030996 <bi3996@icloud.com>
2025-04-25 22:21:50 -0400

    intial commit

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const ProductController = require('../controller/ProductController');
 const { authMiddleWare } = require("../middleware/authMiddleWare");
 
-router.post('/create', authMiddleWare, ProductController.createProduct)
+router.post('/create', ProductController.createProduct)
 router.put('/update/:id', authMiddleWare, ProductController.updateProduct)
-router.get('/get-details/:id',authMiddleWare, ProductController.getDetailProduct)
-router.delete('/delete/:id', authMiddleWare, ProductController.deleteProduct)
+router.get('/get-details/:id', ProductController.getDetailProduct)
+router.delete('/delete/:id', ProductController.deleteProduct)
 router.get('/getAll-product', ProductController.getAllProduct)
 
 module.exports = router
